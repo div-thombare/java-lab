11f2a217d9c3affd117ffdb8a34e8d52a1b7af05
div-thombare <164990221+div-thombare@users.noreply.github.com>
2024-08-21 21:50:39 -0700

    Add files via upload
    Simple exception

---
+++ Division.java
@@ -0,0 +1,21 @@
+class Division
+{
+	public static void main(String args[])
+	{
+		System.out.println("Program is started");
+
+		int a=10,b=0,c;
+		try
+		{
+		c=a/b;
+		System.out.println(c);
+
+		}
+		catch(Exception e)
+		{
+		System.out.println("Envalid");
+		}
+		System.out.println("Program is ended");
+
+	}
+}
